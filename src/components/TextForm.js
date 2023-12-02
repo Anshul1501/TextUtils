bf9dbd1aed54c9c04ce6aedc629561b9b37eccdf
Anshul <a20110100680@gmail.com>
2023-12-02 18:11:41 +0530

    Text Form and Button are added

---
+++ src/components/TextForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextForm(props) {
+  return (
+    <div className="container my-5">
+<div className="mb-3">
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+</div>
+<button type="button" class="btn btn-primary">Convert to Upper Case</button>
+    </div>
+  )
+}
