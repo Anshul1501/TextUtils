d46b5677cdcae9430aa92b9e829e3f9afdcba448
Anshul <a20110100680@gmail.com>
2023-12-03 00:06:49 +0530

    Upper Case Button is added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
+
+  //Handle Upper Case Text
+    const handleUpperCase = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+//Handle On Change 
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const[text, setText] = useState('');
+
   return (
     <div className="container my-5">
 <div className="mb-3">
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
 </div>
-<button type="button" class="btn btn-primary">Convert to Upper Case</button>
+<button type="button" className="btn btn-primary" onClick={handleUpperCase}>Convert to Upper Case</button>
     </div>
   )
 }
